6d428f076ac721248ff395951c9ea231fdfea8c3
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 15:24:42 -0300

    Ubuntu commands normalization and sources

---
+++ recipes/ubuntu.sh
@@ -0,0 +1,15 @@
+[[ `kickstart.os` != "Ubuntu" ]] && return 0
+
+export DEBIAN_FRONTEND=noninteractive
+
+kickstart.apt.upgrade
+kickstart.package.install software-properties-common python-software-properties
+
+code_name=$(kickstart.codename)
+[ -z "$code_name" ] && echo "Couldn't fetch Ubuntu's codename" && exit 1
+
+kickstart.info "Check for presence of universe or add it"
+grep -q "$code_name universe" /etc/apt/sources.list || ( echo deb http://archive.ubuntu.com/ubuntu $code_name universe >> /etc/apt/sources.list )
+
+kickstart.apt.update
+kickstart.apt.upgrade
