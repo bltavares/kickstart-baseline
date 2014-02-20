3db7234f599ff52abb123995ed89ad865ac58262
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 17:45:47 -0300

    Create Ubuntu function to add universe stream

--- recipes/ubuntu.sh
+++ recipes/ubuntu.sh
@@ -6,11 +6,14 @@ export DEBIAN_FRONTEND=noninteractive
 kickstart.apt.upgrade
 kickstart.package.install software-properties-common python-software-properties
 
-code_name=$(kickstart.codename)
-[ -z "$code_name" ] && echo "Couldn't fetch Ubuntu's codename" && exit 1
+baseline.ubuntu.add_universe_package_source() {
+  local code_name=$(kickstart.codename)
+  [ -z "$code_name" ] && echo "Couldn't fetch Ubuntu's codename" && exit 1
 
-kickstart.info "Check for presence of universe or add it"
-grep -q "$code_name universe" /etc/apt/sources.list || ( echo deb http://archive.ubuntu.com/ubuntu $code_name universe >> /etc/apt/sources.list )
+  kickstart.info "Check for presence of universe or add it"
+  grep -q "$code_name universe" /etc/apt/sources.list || ( echo deb http://archive.ubuntu.com/ubuntu $code_name universe >> /etc/apt/sources.list )
 
-kickstart.apt.update
-kickstart.apt.upgrade
+  kickstart.apt.update
+  kickstart.apt.upgrade
+}
+baseline.ubuntu.add_universe_package_source
