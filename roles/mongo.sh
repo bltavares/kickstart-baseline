9e697293c883b3117e534be3745f3f0f125d4aab
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 20:13:30 -0300

    Cleanup execution choices for mongo

--- roles/mongo.sh
+++ roles/mongo.sh
@@ -1,6 +1,6 @@
 kickstart.context "mongo"
 
-baseline.mongo.install.ubuntu() {
+baseline.mongo.install.Ubuntu() {
   echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list
   kickstart.apt.add_key_from_keychain 7F0CEB10
   kickstart.package.update
@@ -10,10 +10,9 @@ baseline.mongo.install.ubuntu() {
   kickstart.service.restart mongodb
 }
 
-baseline.mongo.install.mac() {
+baseline.mongo.install.Mac() {
   kickstart.info "Mongo won't run as service by default on mac"
   kickstart.package.install mongo
 }
 
-kickstart.os.is "Ubuntu" && baseline.mongo.install.ubuntu
-kickstart.os.is "Mac" && baseline.mongo.install.mac
+baseline.mongo.install.`kickstart.os`
