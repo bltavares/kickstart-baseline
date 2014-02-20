4addd46d4315d50f84d0ee4978edb9e6648247bc
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 17:38:45 -0300

    Cleanup racket role variables

--- roles/racket.sh
+++ roles/racket.sh
@@ -1,18 +1,21 @@
-kickstart.context "sml"
+kickstart.context "racket"
 
-racket_dest=racket_install.sh
+baseline.racket.install() {
+  local racket_dest=racket_install.sh
 
-check_racket_integrity() {
-  [ -f $racket_dest ]
-}
+  baseline.racket.check_integrity() {
+    [ -f $racket_dest ]
+  }
 
-install_racket() {
-  echo -e "no\\n4\\n\\n" | kickstart.mute "sh $racket_dest"
-}
+  baseline.racket.install.racket() {
+    echo -e "no\\n4\\n\\n" | kickstart.mute "sh $racket_dest"
+  }
 
-(
+  (
   cd /opt
-  check_racket_integrity || kickstart.download.file http://download.racket-lang.org/installers/5.92/racket-5.92-x86_64-linux-ubuntu-quantal.sh $racket_dest
-  [ `which racket` ] || install_racket
-)
-kickstart.path.add_to_profile.d racket.sh
+  baseline.racket.check_integrity || kickstart.download.file http://download.racket-lang.org/installers/5.92/racket-5.92-x86_64-linux-ubuntu-quantal.sh $racket_dest
+  [ `which racket` ] || baseline.racket.install.racket
+  )
+  kickstart.path.add_to_profile.d racket.sh
+}
+baseline.racket.install
