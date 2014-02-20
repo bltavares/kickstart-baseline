3dad9836e2e771a17201a7caee9abe3bddd514da
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 19:59:20 -0300

    Support mongo on mac

--- roles/mongo.sh
+++ roles/mongo.sh
@@ -1,9 +1,19 @@
 kickstart.context "mongo"
 
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list
-kickstart.apt.add_key_from_keychain 7F0CEB10
-kickstart.apt.update
+baseline.mongo.install.ubuntu() {
+  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list
+  kickstart.apt.add_key_from_keychain 7F0CEB10
+  kickstart.apt.update
 
-kickstart.package.install mongodb-10gen
-kickstart.service.enable mongodb
-kickstart.service.restart mongodb
+  kickstart.package.install mongodb-10gen
+  kickstart.service.enable mongodb
+  kickstart.service.restart mongodb
+}
+
+baseline.mongo.install.mac() {
+  kickstart.info "Mongo won't run as service by default on mac"
+  kickstart.package.install mongo
+}
+
+kickstart.os.is "Ubuntu" && baseline.mongo.install.ubuntu
+kickstart.os.is "Mac" && baseline.mongo.install.mac
