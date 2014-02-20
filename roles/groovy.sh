f6c91e644366f9c81233e86209df624be6aeefe1
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 18:03:28 -0300

    Cleanup groovy variables

--- roles/groovy.sh
+++ roles/groovy.sh
@@ -1,16 +1,19 @@
 source roles/java.sh
 kickstart.context "groovy"
 
-groovy_basename='groovy-2.2.1'
-groovy_tarball="${groovy_basename}.zip"
+baseline.groovy.install() {
+  local groovy_basename='groovy-2.2.1'
+  local groovy_tarball="${groovy_basename}.zip"
 
-kickstart.package.install unzip
+  kickstart.package.install unzip
 
-(
-cd /opt
-[ -f $groovy_tarball ] || kickstart.download.file http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.1.zip $groovy_tarball
-kickstart.mute "unzip -o $groovy_tarball"
-rm -rf groovy 2> /dev/null
-ln -s $groovy_basename groovy
-)
-kickstart.add_to_profile.d groovy.sh
+  (
+  cd /opt
+  [ -f $groovy_tarball ] || kickstart.download.file http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.1.zip $groovy_tarball
+  kickstart.mute "unzip -o $groovy_tarball"
+  rm -rf groovy 2> /dev/null
+  ln -s $groovy_basename groovy
+  )
+  kickstart.add_to_profile.d groovy.sh
+}
+baseline.groovy.install
