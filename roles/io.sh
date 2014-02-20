db14aa61505c94d6eed0d3bbab52bae236250131
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 18:04:15 -0300

    Cleanup io variables

--- roles/io.sh
+++ roles/io.sh
@@ -1,11 +1,14 @@
 kickstart.context "io"
 
-io_dest=iobin-linux-x64-deb-current.zip
+baseline.io.install() {
+  local io_dest=iobin-linux-x64-deb-current.zip
 
-(
+  (
   cd /opt
   [ -f $io_dest ] || kickstart.download.file http://iobin.suspended-chord.info/linux/iobin-linux-x64-deb-current.zip $io_dest
   kickstart.package.install unzip
   kickstart.mute "unzip $io_dest -d io_lang_current"
   dpkg -i io_lang_current/*.deb
-)
+  )
+}
+baseline.io.install
