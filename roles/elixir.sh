7d46b7e670d4a214e6e1c8774a9001a81d8e7250
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 17:59:53 -0300

    Cleanup elixir variables and put on namespaces

--- roles/elixir.sh
+++ roles/elixir.sh
@@ -1,12 +1,15 @@
 source roles/erlang.sh
 kickstart.context "elixir"
 
-elixir_tarball='v0.11.2.zip'
-kickstart.package.install unzip
+baseline.elixir.install() {
+  local elixir_tarball='v0.11.2.zip'
+  kickstart.package.install unzip
 
-(
+  (
   cd /opt
   [ -f $elixir_tarball ] || kickstart.download.file "https://github.com/elixir-lang/elixir/releases/download/v0.11.2/${elixir_tarball}" $elixir_tarball
   kickstart.mute "unzip -o $elixir_tarball -d elixir"
-)
-kickstart.add_to_profile.d elixir.sh
+  )
+  kickstart.add_to_profile.d elixir.sh
+}
+baseline.elixir.install
