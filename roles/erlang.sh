b197392add09d2099bb8c1c73f16cdfd2cb31521
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 20:12:03 -0300

    Introduce erlang support on mac

--- roles/erlang.sh
+++ roles/erlang.sh
@@ -1,6 +1,14 @@
 kickstart.context "erlang"
 
-echo 'deb http://binaries.erlang-solutions.com/debian precise contrib' > /etc/apt/sources.list.d/erlang.list
-kickstart.apt.add_key_from_url http://binaries.erlang-solutions.com/debian/erlang_solutions.asc
-kickstart.apt.update
-kickstart.package.install esl-erlang
+baseline.erlang.install.Ubuntu() {
+  echo 'deb http://binaries.erlang-solutions.com/debian precise contrib' > /etc/apt/sources.list.d/erlang.list
+  kickstart.apt.add_key_from_url http://binaries.erlang-solutions.com/debian/erlang_solutions.asc
+  kickstart.package.update
+  kickstart.package.install esl-erlang
+}
+
+baseline.erlang.install.Mac() {
+  kickstart.package.install erlang
+}
+
+baseline.erlang.install.`kickstart.os`
