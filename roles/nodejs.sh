4dd28545c57bf5e90889c6b491ded2c58e0a9ed3
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 19:28:43 -0300

    Support nodejs on mac

--- roles/nodejs.sh
+++ roles/nodejs.sh
@@ -1,9 +1,5 @@
-kickstart.context "nodejs"
+kickstart.context nodejs
 
-if [ `kickstart.os` == "Ubuntu" ]; then
-  kickstart.apt.ppa ppa:chris-lea/node.js
-  kickstart.package.install nodejs
-else
-  kickstart.info "No instructions for: " `kickstart.os`
-fi
+[[ `kickstart.os` == "Ubuntu" ]] && kickstart.apt.ppa ppa:chris-lea/node.js
+kickstart.package.install nodejs
 
