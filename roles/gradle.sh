ebeddde4681d034e2783c81030dfd4902c4546b9
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 18:02:08 -0300

    Cleanup gradle variables

--- roles/gradle.sh
+++ roles/gradle.sh
@@ -1,14 +1,17 @@
 source roles/java.sh
 kickstart.context "gradle"
 
-gradle_basename='gradle-1.9'
-kickstart.package.install unzip
+baseline.gradle.install() {
+  local gradle_basename='gradle-1.9'
+  kickstart.package.install unzip
 
-(
-cd /opt
-kickstart.download.file "http://services.gradle.org/distributions/${gradle_basename}-bin.zip" "${gradle_basename}.zip"
-kickstart.mute "unzip -o ${gradle_basename}.zip"
-rm -rf gradle 2> /dev/null
-ln -s $gradle_basename gradle
-)
-kickstart.add_to_profile.d gradle.sh
+  (
+  cd /opt
+  kickstart.download.file "http://services.gradle.org/distributions/${gradle_basename}-bin.zip" "${gradle_basename}.zip"
+  kickstart.mute "unzip -o ${gradle_basename}.zip"
+  rm -rf gradle 2> /dev/null
+  ln -s $gradle_basename gradle
+  )
+  kickstart.add_to_profile.d gradle.sh
+}
+baseline.gradle.install
