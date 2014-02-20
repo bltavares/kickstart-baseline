59f4ee41c2308598aafa54673bd1084e2a925309
Bruno Tavares <bruno.exz@gmail.com>
2014-02-20 18:01:09 -0300

    Add go to namespace

--- roles/go.sh
+++ roles/go.sh
@@ -1,11 +1,14 @@
 kickstart.context "go"
 
-download_go() {
-  kickstart.download.stream https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz | tar xz
-}
+baseline.go.install() {
+  baseline.go.download() {
+    kickstart.download.stream https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz | tar xz
+  }
 
-(
+  (
   cd /opt
-  [ -d go ] || download_go
-)
-kickstart.add_to_profile.d go.sh
+  [ -d go ] || baseline.go.download
+  )
+  kickstart.add_to_profile.d go.sh
+}
+baseline.go.install
