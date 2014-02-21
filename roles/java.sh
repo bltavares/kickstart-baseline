bd33fa1839a149d4a6e20034692834681471c59e
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 15:30:32 -0300

    Install maven and ant on mac when java is requested

--- roles/java.sh
+++ roles/java.sh
@@ -1,14 +1,13 @@
-! kickstart.os.is Ubuntu && return
 kickstart.context java
 
-baseline.java.install.java() {
+baseline.java.install.java.Ubuntu() {
   kickstart.apt.ppa ppa:webupd8team/java
   echo debconf shared/accepted-oracle-license-v1-1 select true | kickstart.mute debconf-set-selections
   echo debconf shared/accepted-oracle-license-v1-1 seen true | kickstart.mute debconf-set-selections
   kickstart.package.install oracle-java7-installer
 }
 
-baseline.java.install.maven() {
+baseline.java.install.maven.Ubuntu() {
   local maven_basename='apache-maven-3.1.1'
   local maven_url="http://www.apache.org/dist/maven/maven-3/3.1.1/binaries/${maven_basename}-bin.tar.gz"
 
@@ -21,8 +20,11 @@ baseline.java.install.maven() {
   kickstart.profile.add_to_profile maven.sh
 }
 
+baseline.java.install.maven.Mac() {
+  kickstart.package.install maven
+}
 
-baseline.java.install.ant() {
+baseline.java.install.ant.Ubuntu() {
   local ant_basename='apache-ant-1.9.3'
   local ant_url="http://www.apache.org/dist/ant/binaries/${ant_basename}-bin.tar.gz"
   (
@@ -34,6 +36,10 @@ baseline.java.install.ant() {
   kickstart.profile.add_to_profile ant.sh
 }
 
-baseline.java.install.java
-baseline.java.install.maven
-baseline.java.install.ant
+baseline.java.install.ant.Mac() {
+  kickstart.package.install ant
+}
+
+kickstart.os.is Ubuntu && baseline.java.install.java.Ubuntu
+baseline.java.install.maven.`kickstart.os`
+baseline.java.install.ant.`kickstart.os`
