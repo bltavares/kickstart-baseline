e55d0a3c6c5d325eede9ce39ec17429fa99c60fa
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 11:22:15 -0300

    Add rust support to mac

--- roles/rust.sh
+++ roles/rust.sh
@@ -1,14 +1,22 @@
 kickstart.context "rust"
 
-rust_dest=rust_0.8-1_amd64.deb
-rust_md5='7f2e19bc5cde47e143b7484ecda0207f'
+baseline.rust.install.Ubuntu() {
+  rust_dest=rust_0.8-1_amd64.deb
+  rust_md5='7f2e19bc5cde47e143b7484ecda0207f'
 
-check_rust_integrity() {
-  [ -f $rust_dest ] && ( md5sum $rust_dest | grep -q $rust_md5 ) 
+  check_rust_integrity() {
+    [ -f $rust_dest ] && ( md5sum $rust_dest | grep -q $rust_md5 )
+  }
+
+  (
+  cd /opt
+  check_rust_integrity || kickstart.download.file https://copy.com/voMAE7mI19Jf $rust_dest
+  dpkg -i $rust_dest
+  )
 }
 
-(
-cd /opt
-check_rust_integrity || kickstart.download.file https://copy.com/voMAE7mI19Jf $rust_dest
-dpkg -i $rust_dest
-)
+baseline.rust.install.Mac() {
+  kickstart.package.install rust
+}
+
+baseline.rust.install.`kickstart.os`
