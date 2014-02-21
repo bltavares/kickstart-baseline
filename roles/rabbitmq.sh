0edf606e1dcc5b09089707b0afbdcc11026edc48
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 11:14:01 -0300

    Add rabbitmq support to mac

--- roles/rabbitmq.sh
+++ roles/rabbitmq.sh
@@ -1,9 +1,17 @@
 kickstart.context "rabbitmq"
 
-echo 'deb http://www.rabbitmq.com/debian/ stable main' > /etc/apt/sources.list.d/rabbitmq.list
-kickstart.apt.add_key_from_url http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-kickstart.package.update
-kickstart.package.install rabbitmq-server
+kickstart.rabbitmq.install.Ubuntu() {
+  echo 'deb http://www.rabbitmq.com/debian/ stable main' > /etc/apt/sources.list.d/rabbitmq.list
+  kickstart.apt.add_key_from_url http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+  kickstart.package.update
+  kickstart.package.install rabbitmq-server
 
-kickstart.service.enable rabbitmq-server
-kickstart.service.restart rabbitmq-server
+  kickstart.service.enable rabbitmq-server
+  kickstart.service.restart rabbitmq-server
+}
+
+kickstart.rabbitmq.install.Mac() {
+  kickstart.package.install rabbitmq
+}
+
+kickstart.rabbitmq.install.`kickstart.os`
