ccbdf550f003b7f7ce67c4f069a04c83e3638237
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 11:33:56 -0300

    Add tools support to mac

--- roles/tools.sh
+++ roles/tools.sh
@@ -1,3 +1,9 @@
 kickstart.context "tools"
 
-kickstart.package.install aptitude curl emacs gawk mercurial vim zsh tmux exuberant-ctags git-core
+baseline.tools.packages() {
+  kickstart.os.is Ubuntu && echo git-core exuberant-ctags aptitude
+  kickstart.os.is Mac && echo git ctags
+  echo curl emacs gawk mercurial vim zsh tmux
+}
+
+kickstart.package.install `baseline.tools.packages`
