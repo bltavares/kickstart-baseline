94421a7ff22b9c3c9a34bd4259839863a3500ba3
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 11:28:13 -0300

    Add sml support to mac

--- roles/sml.sh
+++ roles/sml.sh
@@ -1,12 +1,20 @@
 source recipes/gcc.sh
 kickstart.context "sml"
 
-kickstart.package.install gcc-multilib g++-multilib
+baseline.sml.install.Ubuntu() {
+  kickstart.package.install gcc-multilib g++-multilib
 
-mkdir -p /opt/sml
-(
+  mkdir -p /opt/sml
+  (
   cd /opt/sml
   [ -d config ] || kickstart.download.stream http://www.smlnj.org/dist/working/110.74/config.tgz | tar xz
   kickstart.command_exists sml || kickstart.mute config/install.sh
-)
-kickstart.add_to_profile.d sml.sh
+  )
+  kickstart.add_to_profile.d sml.sh
+}
+
+baseline.sml.install.Mac() {
+  kickstart.package.install smlnj
+}
+
+baseline.sml.install.`kickstart.os`
