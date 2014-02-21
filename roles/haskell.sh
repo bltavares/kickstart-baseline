6485f70e213d02170fe2948686763a3df406de00
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 14:05:47 -0300

    Add ~/.cabal/bin to the path on Mac as well

--- roles/haskell.sh
+++ roles/haskell.sh
@@ -1,4 +1,4 @@
 kickstart.context haskell
 
 kickstart.package.install haskell-platform
-kickstart.os.is Ubuntu && kickstart.profile.add_to_profile cabal.sh
+kickstart.profile.add_to_profile cabal.sh
