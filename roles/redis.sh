8019e34f1821c540f5df96cf81e1bb927bd3c871
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 11:18:27 -0300

    Add redis support to mac

--- roles/redis.sh
+++ roles/redis.sh
@@ -1,9 +1,15 @@
 kickstart.context "redis"
 
-if [ `kickstart.os` == "Ubuntu" ]; then
+baseline.redis.install.Ubuntu() {
   kickstart.apt.ppa ppa:chris-lea/redis-server
   kickstart.package.install redis-server
   cp files/redis.conf /etc/redis/redis.conf
   kickstart.service.enable redis-server
   kickstart.service.restart redis-server
-fi
+}
+
+baseline.redis.install.Mac() {
+  kickstart.package.install redis
+}
+
+baseline.redis.install.`kickstart.os`
