329885913072df0a7beb392f964f41afaddf48fb
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 10:48:18 -0300

    Add go support for mac

--- roles/go.sh
+++ roles/go.sh
@@ -1,6 +1,6 @@
 kickstart.context "go"
 
-baseline.go.install() {
+baseline.go.install.Ubuntu() {
   baseline.go.download() {
     kickstart.download.stream https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz | tar xz
   }
@@ -11,4 +11,9 @@ baseline.go.install() {
   )
   kickstart.add_to_profile.d go.sh
 }
-baseline.go.install
+
+baseline.go.install.Mac() {
+  kickstart.package.install go
+}
+
+baseline.go.install.`kickstart.os`
