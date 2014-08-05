dd5938d4d6d8b320ad3f2fc438e2f0de66b9b2cf
Bruno Tavares <connect+github@bltavares.com>
2014-08-05 20:46:24 -0300

    Update Go version

--- roles/go.sh
+++ roles/go.sh
@@ -2,7 +2,7 @@ kickstart.context go
 
 baseline.go.install.Ubuntu() {
   baseline.go.download() {
-    kickstart.download.stream https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz | tar xz
+    kickstart.download.stream http://golang.org/dl/go1.3.linux-amd64.tar.gz || tar xz
   }
 
   (
