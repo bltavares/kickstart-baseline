4530abcddf434cd208a9bf3bf49ff829afb0092d
Bruno Tavares <connect+github@bltavares.com>
2014-08-05 21:39:30 -0300

    Typo on streaming the file

--- roles/go.sh
+++ roles/go.sh
@@ -2,7 +2,7 @@ kickstart.context go
 
 baseline.go.install.Ubuntu() {
   baseline.go.download() {
-    kickstart.download.stream http://golang.org/dl/go1.3.linux-amd64.tar.gz || tar xz
+    kickstart.download.stream http://golang.org/dl/go1.3.linux-amd64.tar.gz | tar xz
   }
 
   (
